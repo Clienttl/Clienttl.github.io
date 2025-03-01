f5f8a817d53b8fd61222e389c26633a7ab9aa72b
Clienttl <highridess@outlook.com>
2025-02-28 21:43:02 -0500

    Update script.js

--- bp/script.js
+++ bp/script.js
@@ -1,27 +1,24 @@
 function buypass() {
-    if (!window.PaymentRequest) {
-        return alert("Payment Request API is not supported.");
-    }
+  if (!window.PaymentRequest) return alert("Payment Request APIã«æœªå¯¾å¿œãªãŸã‚ä½¿ãˆã¾ã›ã‚“");
 
-    new PaymentRequest(
-        [
-            {
-                supportedMethods: location.origin + "/payment-manifest.json",
-                data: {
-                    url: document.querySelector("input").value
-                }
-            }
-        ],
-        {
-            total: {
-                label: "_",
-                amount: {
-                    value: "1",
-                    currency: "USD"
-                }
-            }
-        }
-    ).show();
+  new PaymentRequest(
+    [
+      {
+        supportedMethods: location.origin + "/payment-manifest.json",
+        data: {
+          url: document.querySelector("input").value
+        },
+      },
+    ],
+    {
+      total: {
+        label: "_",
+        amount: {
+          value: "1", currency: "USD"
+        },
+      },
+    }
+  ).show();
 }
 
 document.querySelector("button").onclick = buypass;
